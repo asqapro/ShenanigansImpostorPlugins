8f08c3c7e4bd6f951836210433d734c0a446e084
zanderman112 <zanderman4513@gmail.com>
2021-01-03 01:55:31 -0600

    Initial check-in of the Bomber plugin

---
+++ Bomber/Bomber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Impostor.Api.Events.Managers;
+using Impostor.Api.Plugins;
+using Impostor.Plugins.Bomber.Handlers;
+using Microsoft.Extensions.Logging;
+
+namespace Impostor.Plugins.Bomber
+{
+    [ImpostorPlugin(
+        package: "gg.impostor.Bomber",
+        name: "Example",
+        author: "AeonLucid",
+        version: "1.0.0")]
+    public class Bomber : PluginBase
+    {
+        private readonly ILogger<Bomber> _logger;
+        private readonly IEventManager _eventManager;
+        private IDisposable _unregister;
+
+        public Bomber(ILogger<Bomber> logger, IEventManager eventManager)
+        {
+            _logger = logger;
+            _eventManager = eventManager;
+        }
+
+        public override ValueTask EnableAsync()
+        {
+            _logger.LogInformation("Bomber is being enabled.");
+            _unregister = _eventManager.RegisterListener(new GameEventListener(_logger));
+            return default;
+        }
+
+        public override ValueTask DisableAsync()
+        {
+            _logger.LogInformation("Bomber is being disabled.");
+            _unregister.Dispose();
+            return default;
+        }
+    }
+}
---
+++ Bomber/GameEventListener.cs
@@ -0,0 +1,123 @@
+﻿using Impostor.Api.Events;
+using Impostor.Api.Events.Player;
+using Impostor.Api.Innersloth;
+using Impostor.Api.Net.Inner.Objects;
+using Impostor.Api.Games;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Timers;
+
+namespace Impostor.Plugins.Bomber.Handlers
+{
+    public class GameEventListener : IEventListener
+    {
+        private readonly ILogger<Bomber> _logger;
+
+        Dictionary<Api.Net.IClientPlayer, Timer> bombMap;
+
+        public GameEventListener(ILogger<Bomber> logger)
+        {
+            _logger = logger;
+            bombMap = new Dictionary<Api.Net.IClientPlayer, Timer>();
+        }
+
+        private void bombGoBoom(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Timer timer = (Timer) source;
+            foreach(var x in bombMap)
+            {
+                if(x.Value.Equals(timer))
+                {
+                    x.Key.Character.SetExiledAsync(x.Key);
+                }
+            }  
+        }
+
+        [EventListener]
+        public void OnGameStarted(IGameStartedEvent e)
+        {
+            //modify this later once we can set plugin options via chat
+            Random r = new Random();
+            var player = e.Game.Players.ElementAt(r.Next(0, e.Game.PlayerCount-1));
+
+            while(player.Character.PlayerInfo.IsImpostor)
+            {
+                player = e.Game.Players.ElementAt(r.Next(0, e.Game.PlayerCount-1));
+            }
+            
+            //start timers for 25 seconds
+            bombMap.Add(player, new Timer());
+            bombMap.GetValueOrDefault(player).Elapsed += bombGoBoom;
+            bombMap.GetValueOrDefault(player).Interval = 2500;
+            bombMap.GetValueOrDefault(player).Start();
+        }
+
+        public void OnTaskComplete(IPlayerCompletedTaskEvent e)
+        {
+            if(bombMap.ContainsKey(e.ClientPlayer))
+            {
+                bombMap.GetValueOrDefault(e.ClientPlayer).Stop();
+
+                foreach(var task in e.ClientPlayer.Character.PlayerInfo.Tasks)
+                {
+                    if(!task.Complete)
+                    {
+                        //set timer to 20 seconds and restart
+                        bombMap.GetValueOrDefault(e.ClientPlayer).Interval = 2000;
+                        bombMap.GetValueOrDefault(e.ClientPlayer).Start();
+                        return;
+                    }
+                }
+            }
+        }
+
+        
+        [EventListener]
+        public void OnPlayerChat(IPlayerChatEvent e)
+        {
+            if(e.Game.GameState == GameStates.NotStarted)
+            {
+                //call command parser
+
+                //configure number of bombers
+            }
+        }
+
+        bool validateCommand(string[] commandParts)
+        {
+            if(commandParts.Length == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private async ValueTask ServerMessage(IInnerPlayerControl sender, IInnerPlayerControl receiver, String message)
+        {
+            var currentColor = sender.PlayerInfo.ColorId;
+            var currentName = sender.PlayerInfo.PlayerName;
+
+            await sender.SetColorAsync(Impostor.Api.Innersloth.Customization.ColorType.White);
+            await sender.SetNameAsync("Server");
+
+            await sender.SendChatToPlayerAsync(message, receiver);
+
+            await sender.SetColorAsync(currentColor);
+            await sender.SetNameAsync(currentName);
+        }
+
+        public static Boolean isAlphaNumeric(string strToCheck)
+        {
+            Regex rg = new Regex(@"^[a-zA-Z0-9\.]*$");
+            return rg.IsMatch(strToCheck);
+        }
+    }
+}
