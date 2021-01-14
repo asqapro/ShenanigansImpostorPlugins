14ef8645b476e5b82640371b127edc2870496ba7
Adam Whisnant <asqapro11@gmail.com>
2021-01-14 02:55:00 -0600

    Removed unnecessary shuffling

--- SpecialRoles/GameEventListener.cs
+++ SpecialRoles/GameEventListener.cs
@@ -9,23 +9,6 @@ using Roles;
 
 namespace Impostor.Plugins.SpecialRoles.Handlers
 {
-
-    public static class Shuffler
-    {
-        private static Random rng = new Random();  
-        public static void Shuffle<T>(this IList<T> list)  
-        {
-            int n = list.Count;
-            while (n > 1) {
-                n--;
-                int k = rng.Next(n + 1);
-                T value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
-    }
-
     /// <summary>
     ///     A class that listens for two events.
     ///     It may be more but this is just an example.
