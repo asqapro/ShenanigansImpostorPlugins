31648c1c614ef7eb5a4474f36d71c3aa272b4f61
Adam Whisnant <asqapro11@gmail.com>
2021-01-15 03:22:09 -0600

    Got Infected plugin working in most basic way. WIP

--- Infected/GameEventListener.cs
+++ Infected/GameEventListener.cs
@@ -1,71 +1,52 @@
-﻿using Impostor.Api.Events;
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using Impostor.Api.Events;
 using Impostor.Api.Events.Player;
 using Impostor.Api.Net.Inner.Objects;
-using Impostor.Api.Games;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+
 
 namespace Impostor.Plugins.Infected.Handlers
 {
     public class GameEventListener : IEventListener
     {
         private readonly ILogger<Infected> _logger;
+        private HashSet<byte> infected;
 
         public GameEventListener(ILogger<Infected> logger)
         {
             _logger = logger;
+            infected = new HashSet<byte>();
         }
 
-        public bool someoneDied = false;
-        public IInnerPlayerControl whoDied;
+        private void setInfected(IInnerPlayerControl toInfect)
+        {
+            toInfect.SetNameAsync("Infected");
+            toInfect.SetHatAsync(84);
+            toInfect.SetColorAsync(2);
+            toInfect.SetSkinAsync(0f);
+            infected.Add(toInfect.PlayerId);
+        }
 
         [EventListener]
         public void OnGameStarted(IGameStartedEvent e)
         {
             _logger.LogInformation($"Game is starting.");
-            // This prints out for all players if they are impostor or crewmate.
-            Random rd = new Random();
-            int rd_num = rd.Next(0, e.Game.PlayerCount - e.Game.Options.NumImpostors - 1);
-            int rd_perm_num = rd_num;
+            var rng = new Random();
+            var initialInfectedPlayer = rng.Next(0, e.Game.PlayerCount);
+            var playerIdx = 0;
             foreach (var player in e.Game.Players)
             {
-                var info = player.Character.PlayerInfo;
-                var playerEdit = player.Character;
-                var isImpostor = info.IsImpostor;
-                if (isImpostor)
+                if (playerIdx == initialInfectedPlayer)
                 {
-                    if (rd_num == player.Client.Id)
-                    {
-                        rd_num = rd.Next(0, e.Game.PlayerCount - e.Game.Options.NumImpostors - 1);
-                    }
-                    _logger.LogInformation($"- {info.PlayerName} is an impostor.");
-                    playerEdit.SetHatAsync(84);
-                    playerEdit.SetColorAsync(2);
-                    playerEdit.SetSkinAsync(0f);
-                    playerEdit.SetNameAsync("Infected");
+                    setInfected(player.Character);
+                    var newSetting = e.Game;
+                    newSetting.Options.CrewLightMod -= 0.50f;
+                    newSetting.SyncSettingsAsync(player.Client.Player);
+                    break;
                 }
-                else
-                {
-                    if (rd_perm_num == player.Client.Id)
-                    {
-                        playerEdit.SetNameAsync("Crewmates chief");
-                        playerEdit.SetColorAsync(1);
-                        playerEdit.SetHatAsync(13);
-                        playerEdit.SetSkinAsync(4);
-                    }
-                    else
-                    {
-                        playerEdit.SetHatAsync(10);
-                        playerEdit.SetColorAsync(10);
-                        playerEdit.SetSkinAsync(0f);
-                        playerEdit.SetNameAsync("Crewmate");
-                    }
-                    _logger.LogInformation($"- {info.PlayerName} is a crewmate.");
-                    _logger.LogInformation($"{rd_perm_num} is number 1");
-                    _logger.LogInformation($"{player.Client.Id} is the id");
-                }
+                playerIdx++;
             }
         }
 
@@ -80,22 +61,41 @@ namespace Impostor.Plugins.Infected.Handlers
         {
             _logger.LogInformation($"{e.PlayerControl.PlayerInfo.PlayerName} said {e.Message}");
         }
+
         [EventListener]
         public void OnGameCreated(IGameCreatedEvent e)
         {
             _logger.LogInformation($"Game created");
         }
+
         [EventListener]
-        public void OnPlayerMurder(IPlayerMurderEvent e)
+        public void OnPlayerMovement(IPlayerMovementEvent e)
         {
-            _logger.LogInformation($"killed {e.Victim.PlayerInfo.PlayerName}");
-            e.Victim.SetNameAsync("Infected");
-            e.Victim.SetHatAsync(84);
-            e.Victim.SetColorAsync(2);
-            e.ClientPlayer.Game.Options.NumImpostors++;
-            List<IInnerPlayerControl> pInfected = new List<IInnerPlayerControl>();
-            pInfected.Add(e.Victim);
-            e.Game.SetInfectedAsync(pInfected);
+            foreach (var player in e.Game.Players)
+            {
+                if (player.Client.Id == e.ClientPlayer.Client.Id)
+                {
+                    continue;
+                }
+                var distance = Vector2.Distance(e.PlayerControl.NetworkTransform.Position, player.Character.NetworkTransform.Position);
+                if (distance < 0.6 && (infected.Contains(e.PlayerControl.PlayerId) || infected.Contains(player.Character.PlayerId)))
+                {
+                    if (!infected.Contains(e.PlayerControl.PlayerId))
+                    {
+                        setInfected(e.PlayerControl);
+                        var newSetting = e.Game;
+                        newSetting.Options.CrewLightMod -= 0.50f;
+                        newSetting.SyncSettingsAsync(e.ClientPlayer);
+                    }
+                    if (!infected.Contains(player.Character.PlayerId))
+                    {
+                        setInfected(player.Character);
+                        var newSetting = e.Game;
+                        newSetting.Options.CrewLightMod -= 0.50f;
+                        newSetting.SyncSettingsAsync(player.Client.Player);
+                    }
+                }
+            }
         }
     }
 }
