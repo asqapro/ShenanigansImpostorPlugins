c49013904b51c47d36ed81eb0382d8574aa36f59
Adam Whisnant <asqapro11@gmail.com>
2021-01-15 21:01:08 -0600

    Standardizer infecting players. Reset infected player looks post-game

--- Infected/GameEventListener.cs
+++ Infected/GameEventListener.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Numerics;
 using System.Collections.Generic;
+using Impostor.Api.Games;
 using Impostor.Api.Events;
 using Impostor.Api.Events.Player;
+using Impostor.Api.Net;
 using Impostor.Api.Net.Inner.Objects;
 using Microsoft.Extensions.Logging;
 
@@ -12,27 +14,42 @@ namespace Impostor.Plugins.Infected.Handlers
     public class GameEventListener : IEventListener
     {
         private readonly ILogger<Infected> _logger;
-        private HashSet<byte> infected;
+        private Dictionary<GameCode, HashSet<IClientPlayer>> infected;
 
         public GameEventListener(ILogger<Infected> logger)
         {
             _logger = logger;
-            infected = new HashSet<byte>();
+            infected = new Dictionary<GameCode, HashSet<IClientPlayer>>();
         }
 
-        private void setInfected(IInnerPlayerControl toInfect)
+        private void setInfected(IClientPlayer toInfect, IGame game)
         {
-            toInfect.SetNameAsync("Infected");
-            toInfect.SetHatAsync(84);
-            toInfect.SetColorAsync(2);
-            toInfect.SetSkinAsync(0f);
-            infected.Add(toInfect.PlayerId);
+            toInfect.Character.SetNameAsync("Infected");
+            toInfect.Character.SetHatAsync(84);
+            toInfect.Character.SetColorAsync(2);
+            toInfect.Character.SetSkinAsync(0f);
+            infected[game.Code].Add(toInfect);
+
+            var moddedOptions = game;
+            moddedOptions.Options.CrewLightMod = 0.25f;
+            moddedOptions.Options.ImpostorLightMod = 0.25f;
+            moddedOptions.Options.PlayerSpeedMod = 1.25f;
+            moddedOptions.SyncSettingsToAsync(toInfect);
         }
 
         [EventListener]
         public void OnGameStarted(IGameStartedEvent e)
         {
             _logger.LogInformation($"Game is starting.");
+
+            infected[e.Game.Code] = new HashSet<IClientPlayer>();
+
+            e.Game.Options.ImpostorLightMod = 1.0f;
+            e.Game.Options.CrewLightMod = 1.0f;
+            e.Game.Options.KillCooldown = 1000;
+            e.Game.Options.PlayerSpeedMod = 2.0f;
+            e.Game.SyncSettingsAsync();
+
             var rng = new Random();
             var initialInfectedPlayer = rng.Next(0, e.Game.PlayerCount);
             var playerIdx = 0;
@@ -40,10 +57,7 @@ namespace Impostor.Plugins.Infected.Handlers
             {
                 if (playerIdx == initialInfectedPlayer)
                 {
-                    setInfected(player.Character);
-                    var newSetting = e.Game;
-                    newSetting.Options.CrewLightMod -= 0.50f;
-                    newSetting.SyncSettingsAsync(player.Client.Player);
+                    setInfected(player.Client.Player, e.Game);
                     break;
                 }
                 playerIdx++;
@@ -54,6 +68,19 @@ namespace Impostor.Plugins.Infected.Handlers
         public void OnGameEnded(IGameEndedEvent e)
         {
             _logger.LogInformation($"Game has ended.");
+            foreach (var player in e.Game.Players)
+            {
+                IClientPlayer origPlayerInfo = null;
+                infected[e.Game.Code].TryGetValue(player, out origPlayerInfo);
+                if (origPlayerInfo != null)
+                {
+                    player.Character.SetNameAsync(origPlayerInfo.Character.PlayerInfo.PlayerName);
+                    player.Character.SetHatAsync(origPlayerInfo.Character.PlayerInfo.HatId);
+                    player.Character.SetColorAsync(origPlayerInfo.Character.PlayerInfo.ColorId);
+                    player.Character.SetSkinAsync(origPlayerInfo.Character.PlayerInfo.SkinId);
+                }
+            }
+
         }
 
         [EventListener]
@@ -73,26 +100,24 @@ namespace Impostor.Plugins.Infected.Handlers
         {
             foreach (var player in e.Game.Players)
             {
-                if (player.Client.Id == e.ClientPlayer.Client.Id)
+                if (player == null)
+                {
+                    continue;
+                }
+                if (player.Character.PlayerId == e.PlayerControl.PlayerId)
                 {
                     continue;
                 }
                 var distance = Vector2.Distance(e.PlayerControl.NetworkTransform.Position, player.Character.NetworkTransform.Position);
-                if (distance < 0.6 && (infected.Contains(e.PlayerControl.PlayerId) || infected.Contains(player.Character.PlayerId)))
+                if (distance < 0.6)
                 {
-                    if (!infected.Contains(e.PlayerControl.PlayerId))
+                    if (!infected[e.Game.Code].Contains(e.ClientPlayer))
                     {
-                        setInfected(e.PlayerControl);
-                        var newSetting = e.Game;
-                        newSetting.Options.CrewLightMod -= 0.50f;
-                        newSetting.SyncSettingsAsync(e.ClientPlayer);
+                        setInfected(e.ClientPlayer, e.Game);
                     }
-                    if (!infected.Contains(player.Character.PlayerId))
+                    if (!infected[e.Game.Code].Contains(player))
                     {
-                        setInfected(player.Character);
-                        var newSetting = e.Game;
-                        newSetting.Options.CrewLightMod -= 0.50f;
-                        newSetting.SyncSettingsAsync(player.Client.Player);
+                        setInfected(player.Client.Player, e.Game);
                     }
                 }
             }
